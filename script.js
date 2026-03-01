92296b30a066aefd53643d5647c2c78e9fc2f58a
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-03-02 00:25:41 +0600

    Recap Fetch() & Send and Console Each Data

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 const loadData = () => {
   fetch("https://jsonplaceholder.typicode.com/todos/1")
+    //promise of response
     .then((res) => res.json())
+    //promise of json data
     .then((data) => console.log(data));
 };
+
+// const loadPost = ()=>{
+//   const url = "https://jsonplaceholder.typicode.com/posts"
+//   fetch(url)
+//   .then((res)=> res.json())
+//   .then((data)=>console.log(data))
+// }
+
+
+
+
+const loadPost = () => {
+  const url = "https://jsonplaceholder.typicode.com/posts";
+  fetch(url)
+    .then((res) => res.json())
+    .then((json) => displayPost(json));
+};
+
+
+const displayPost = (posts)=>{
+posts.forEach(post => {
+  console.log(post);
+});
+}
+
+
+
+
+
