36680c3e421a0b840d03399f8b6e67e26fcfac5a
MD ABDUR RAHMAN SHOAIB <mdabdurrahmanshoaib@gmail.com>
2026-03-02 17:51:34 +0600

    Display Post Card & Style using CSS

--- post.js
+++ post.js
@@ -5,22 +5,47 @@ const loadPost = () => {
     .then((data) => displayPost(data));
 };
 //array of object
+// const displayPost = (posts) => {
+//   //1. get the container where child is append
+//  const postContainer = document.getElementById("post-container")
+//  postContainer.innerHTML = "";
+
+// posts.forEach(element => {
+
+//   //2. create html element
+//   const li =  document.createElement("li");
+//   li.innerText = element.title;
+
+//   //3. add LI into container.
+//   postContainer.appendChild(li)
+//   });
+// };
+
+// {
+//     "userId": 10,
+//     "id": 94,
+//     "title": "qui qui voluptates illo iste minima",
+//     "body": "aspernatur expedita soluta quo ab ut similique\nexpedita dolores amet\nsed temporibus distinctio magnam saepe deleniti\nomnis facilis nam ipsum natus sint similique omnis"
+// }
+
 const displayPost = (posts) => {
-  //1. get the container where child is append
- const postContainer = document.getElementById("post-container")
-//  console.log(postContainer);
+  //1. get the container and empty the container
+  const postContainer = document.getElementById("post-container");
+  postContainer.innerHTML = "";
+  posts.forEach((post) => {
+    //2. create element
+    const postCard = document.createElement("div");
+    postCard.innerHTML = `
+  <div class="post-card">
+        <h2>${post.title}</h2>
+        <p>
+         ${post.body}
+        </p>
+      </div>
+  `;
 
+  //3. append the child to the container.
+    postContainer.append(postCard)
 
-posts.forEach(element => {
-  // console.log(element.title);
-  
-  //2. create html element 
-  const li =  document.createElement("li");
-  li.innerText = element.title;
-  // console.log(li);
-
-
-  //3. add LI into container.
-  postContainer.appendChild(li)
   });
 };
